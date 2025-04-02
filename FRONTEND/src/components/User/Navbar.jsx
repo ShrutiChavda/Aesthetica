1f140ca3b1bc44c843b412b4c9abc3a6066a1976
Shruti Chavda <schavda684@rku.ac.in>
2025-04-02 13:41:22 +0530

    new

--- FRONTEND/src/components/User/Navbar.jsx
+++ FRONTEND/src/components/User/Navbar.jsx
@@ -1,46 +1,27 @@
 // Navbar.jsx
-import React, { useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
 import logo from '../../assets/images/logo.png';
 import '../../assets/css/user/navbar.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
   return (
     <nav className="navbar">
-      <div className="navbar-left">
-        <Link to="/user/index">
-          <img src={logo} alt="Aesthetica Logo" className="navbar-logo" />
-        </Link>
-      </div>
-
-      <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
-        <i className={menuOpen ? 'bi bi-x' : 'bi bi-list'}></i>
-      </button>
-
-      <div className={`navbar-center ${menuOpen ? 'open' : ''}`}>
-        <ul className="navbar-links">
-          <li><NavLink to="/user/index" onClick={() => setMenuOpen(false)}>Home</NavLink></li>
-          <li><NavLink to="/user/blog" onClick={() => setMenuOpen(false)}>Blog</NavLink></li>
-          <li><NavLink to="/user/style" onClick={() => setMenuOpen(false)}>Styles</NavLink></li>
-          <li><NavLink to="/user/budget" onClick={() => setMenuOpen(false)}>Budget</NavLink></li>
-          <li><NavLink to="/user/room" onClick={() => setMenuOpen(false)}>Measure</NavLink></li>
-          <li><NavLink to="/user/job" onClick={() => setMenuOpen(false)}>Careers</NavLink></li>
+      <Link to="/user/index">
+        <img src={logo} alt="Aesthetica Logo" className="navbar-logo" />
+      </Link>
+      <div className="nav-links">
+        <ul>
+          <li><NavLink to="/user/index" activeClassName="active">Home</NavLink></li>
+          <li><NavLink to="/user/blog" activeClassName="active">Blog</NavLink></li>
+          <li><NavLink to="/user/style" activeClassName="active">Styles</NavLink></li>
+          <li><NavLink to="/user/budget" activeClassName="active">Budget</NavLink></li>
+          <li><NavLink to="/user/room" activeClassName="active">Measure</NavLink></li>
+          <li><NavLink to="/user/job" activeClassName="active">Careers</NavLink></li>
         </ul>
-      </div>
-
-      <div className="navbar-right">
-        <div className="search-bar">
-          <input type="text" placeholder="Search designs, styles, tips" />
-          <button><i className="bi bi-search"></i></button>
-        </div>
-        <div className="navbar-icons">
-          <button><i className="bi bi-bell"></i></button>
-          <Link to="/user/profile">
-            <button><i className="bi bi-person-fill"></i></button>
-          </Link>
+        <div className="auth-buttons">
+          <button className="sn">Sign In</button>
+          <button className="rg">Register</button>
         </div>
       </div>
     </nav>
