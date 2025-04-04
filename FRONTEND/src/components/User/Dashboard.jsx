5df426f5d48ed3b8e434389c4b516f0620b9a328
Urvisha1103 <129959087+Urvisha1103@users.noreply.github.com>
2025-04-04 08:47:56 +0530

    commited

--- FRONTEND/src/components/User/Dashboard.jsx
+++ FRONTEND/src/components/User/Dashboard.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Blog from "./Blog";
+import Style from './Style';
 function Dashboard() {
   return (
     <div className="font-sans text-black bg-white">
     <Blog/>
+    {/* <Style/> */}
+    
     </div>
   )
 }
